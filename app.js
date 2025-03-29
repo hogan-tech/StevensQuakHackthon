1538c32b2833e8bfcda72d7308e51c33acd3964e
Hogan <purmonth@gmail.com>
2025-03-29 18:21:05 -0400

    test app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-import express from 'express';
+import express from "express";
+import cool from "cool-ascii-faces";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Heroku!');
+app.get("/", (req, res) => {
+  res.send("Hello from Heroku!");
+});
+
+app.get("/cool", (req, res) => {
+  console.log(`Rendering a cool ascii face for route '/cool'`);
+  res.send(cool());
 });
 
 app.listen(PORT, () => {
