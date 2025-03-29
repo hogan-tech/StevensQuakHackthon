ed7f89b80f27704afa8677e47fd548760a51046e
Hogan <purmonth@gmail.com>
2025-03-29 18:14:59 -0400

    test app.js

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-import express from "express";
-import constructorMethod from "./routes/index.js";
-import cors from "cors";
-const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-// app.use(cors());
-
-constructorMethod(app);
-
-app.listen(3000, () => {
-    console.log("We've now got a server!");
-    console.log("Your routes will be running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
