beb6f746cd04e7190d79d0b27dd31268ad6decc6
Hogan <purmonth@gmail.com>
2025-03-29 18:06:34 -0400

    add gitignore

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@
 import anxietyRoutes from "./anxiety.js";
 import usersRoute from "./users.js";
 const constructorMethod = (app) => {
-    app.use("/anxiety", anxietyRoutes);
-    app.use("/users", usersRoute);
+    // app.use("/anxiety", anxietyRoutes);
+    // app.use("/users", usersRoute);
     app.use("*", (req, res) => {
-        res.status(404).json({ error: "Not found" });
+        res.status(404).json({ error: "Hello World" });
     });
 };
 
